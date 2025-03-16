bef44de1807cb325b0f6a8fb07e60c8abe7520d2
Juhani Aarni <juhani.aarni@outlook.com>
2025-03-16 21:05:15 +0200

    Add validation messages to UI

--- scripts/ui.py
+++ scripts/ui.py
@@ -10,7 +10,7 @@ root = Tk()
 # root window title and dimension
 root.title("Prime factors finder")
 # Set geometry(widthxheight)
-root.geometry('450x250')
+root.geometry('350x250')
 
 # adding a label to the root window
 lbl = Label(root, text = "Give any positive natural number.")
@@ -24,18 +24,36 @@ txt.grid(column =0, row =1)
 # function to display user text when 
 # button is clicked
 def clicked():
+    # Validate input is a number 
+    validate = prime_factor.validate_given_number(txt.get())
+    if not validate:
+        res = f'Given input is not a positive integer. Please use positive integers only!'
+        lbl.configure(text = res)
+        return
+    
+    # Change the given string input to integer
     number = int(txt.get())
     factors, t1, found_database = prime_factor.find_prime_factors(number)
-    # Save to output file
+
+    # Save to number, factors and time it took to find to a file
     save_file.save_to_ouput_file(number, factors, t1)
-    # Save to database
+
+    # Save the info to database
     if found_database is False:
         factors = list(set(factors))
         database.database_csv().save_to_database(number, factors)
         factors = ', '.join(map(str,factors))
-    
-    #res = f'Prime factors for number {number} are {', '.join(map(str,factors))}\n time it took was {t1:.4f}'
-    res = f'Prime factors for number {number} are {factors}\n time it took was {t1:.4f}'
+
+    # Style the text based on plurlar vs singular
+    print(len(factors.split(',')))
+    if len(factors.split(',')) > 1:
+        factor_plular = 'factors'
+        plular = 'are'
+    else:
+        factor_plular = 'factor'
+        plular = 'is'
+
+    res = f'Prime {factor_plular} for number {number} {plular} {factors}\n Time it took to find those was {t1:.4f}'
     lbl.configure(text = res)
     
 
